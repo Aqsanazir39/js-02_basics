a019b5916bfcb1dc9a03137d6e623801736f74ca
Aqsa Nazir <aqsanazir39@gmail.com>
2024-04-26 13:13:42 +0500

    new file

---
+++ 01_arrays.js
@@ -0,0 +1,9 @@
+//arrays
+
+const myArr = [0, 1, 2, 3, 4, 5];
+console.log(myArr[0]);
+
+const heer = ["hsdjjd" , " fdhjs" , "dmfdm"]
+
+const myArr2 = new Array(1,2,4,5)
+console.log(myArr2.length);
