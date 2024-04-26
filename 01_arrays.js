36b7f63aa8a7e1b37ae0eadc5b4895694d0e926c
Aqsa Nazir <aqsanazir39@gmail.com>
2024-04-26 14:55:41 +0500

    new

--- 01_arrays.js
+++ 01_arrays.js
@@ -5,5 +5,21 @@ console.log(myArr[0]);
 
 const heer = ["hsdjjd" , " fdhjs" , "dmfdm"]
 
-const myArr2 = new Array(1,2,4,5)
-console.log(myArr2.length);
+// const myArr2 = new Array(1,2,4,5)
+// console.log(myArr2.length);
+
+// console.log(myArr);
+myArr.push(6);
+myArr.push(8);
+
+// myArr.unshift(1);
+// console.log(myArr);
+
+// console.log(myArr.includes(9));
+
+// console.log(myArr.indexOf(3 , 4));
+ const newArr = myArr.join()
+ console.log(myArr);
+ console.log(newArr);
+
+
